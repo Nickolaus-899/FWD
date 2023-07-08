b9b55a4026e5de6bf35e702899152d864c5ab54b
unknown <n.petukhov@innopolis.university>
2023-07-08 23:34:43 +0300

    Updates

---
+++ _app/immutable/chunks/paths.5402160e.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_jhtfga)==null?void 0:s.base)??"/Nickolaus-899/FWD";var a;const e=((a=globalThis.__sveltekit_jhtfga)==null?void 0:a.assets)??t;export{e as a,t as b};
