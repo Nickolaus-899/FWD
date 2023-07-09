328ec92eafb97507fa9b35b579c557df5828a0f9
unknown <n.petukhov@innopolis.university>
2023-07-09 10:31:57 +0300

    svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,5 @@
 <script>
     import App from "./App.svelte";
-    import Header from "./Header.svelte";
-    import Footer from "./Footer.svelte";
-    import Enigma from "./enigma/Enigma.svelte";
-    import {Route, Router} from "svelte-routing";
 
     import "./../css/main.css"
 </script>
--- src/routes/App.svelte
+++ src/routes/App.svelte
@@ -1,21 +1,9 @@
 <script>
 import Projects from "./Projects.svelte";
-import Joke from "./comic/Joke.svelte";
-import {Route, Router, navigate} from "svelte-routing";
-import Enigma from "./enigma/Enigma.svelte";
 import ButtonToJoke from "./ButtonToJoke.svelte";
-import {base} from "$app/paths";
-import Header from "./Header.svelte";
-import Footer from "./Footer.svelte";
-import MainPage from "./MainPage.svelte";
 </script>
 
 <div>
-<!--    <Router>-->
-<!--        <Route path="/{base}" component={MainPage}></Route>-->
-<!--        <Route path="/{base}/enigma" component={Enigma}></Route>-->
-<!--        <Route path="/{base}/comic" component={Joke}></Route>-->
-<!--    </Router>-->
     <Projects/>
     <ButtonToJoke/>
 </div>
